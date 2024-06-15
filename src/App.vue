1efb5842e609b9cd367d6c70a79a67e2c8a5cbc1
zimmer <zimmertorrijos@gmail.com>
2024-06-15 22:44:28 +0800

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <router-view />
+</template>
+
+<script setup lang="ts">
+import type { PermissionState } from '@capacitor/core'
+
+defineOptions({
+  name: 'App',
+})
+
+export interface PermissionStatus {
+  // TODO: change 'location' to the actual name of your alias!
+  camera: PermissionState;
+}
+</script>
---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,20 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script setup lang="ts">
+
+defineOptions({
+  name: 'MainLayout',
+})
+
+</script>
+
+<style lang="sass">
+.q-layout
+  background-color: #71b6ef
+</style>

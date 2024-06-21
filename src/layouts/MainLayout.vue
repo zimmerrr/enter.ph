8d1d8aafd1364e3c50aef37b8ff7a58fefb20168
zimmer <zimmertorrijos@gmail.com>
2024-06-21 21:51:43 +0800

    apply diff backgrounds per page

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,5 +1,8 @@
 <template>
-  <q-layout view="lhh Lpr lff">
+  <q-layout
+    view="lhh Lpr lff"
+    :class="route.meta.bgHeader ? route.meta.bgHeader : `` "
+  >
     <Navigation />
     <q-page-container>
       <router-view />
@@ -11,16 +14,43 @@
 <script setup lang="ts">
 import Navigation from 'src/components/Navigation.vue'
 import Footer from 'src/components/Footer.vue'
+import { useRoute } from 'vue-router'
+
 defineOptions({
   name: 'MainLayout',
 })
 
+const route = useRoute()
+const bgHeader = route.meta.bgHeader
+console.log(bgHeader)
 </script>
 
 <style lang="sass">
 .q-layout
   background-color: black
-  background-image: url('assets/home/bg-home.png') !important
+
+.home
+  background-image: url('assets/home/bg.png') !important
+  background-repeat: no-repeat !important
+  background-position: 100% 0% !important
+
+.apparel
+  background-image: url('assets/apparel/bg.png') !important
+  background-repeat: no-repeat !important
+  background-position: 100% 0% !important
+
+.justice
+  background-image: url('assets/justice/bg.png') !important
+  background-repeat: no-repeat !important
+  background-position: 100% 0% !important
+
+.entertainment
+  background-image: url('assets/entertainment/bg.png') !important
+  background-repeat: no-repeat !important
+  background-position: 100% 0% !important
+
+.about
+  background-image: url('assets/about/bg.png') !important
   background-repeat: no-repeat !important
   background-position: 100% 0% !important
 

96d37247142b666e8b630b8db132d1c10ccaa942
zimmer <zimmertorrijos@gmail.com>
2024-06-21 21:55:13 +0800

    fix naming error

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -4,7 +4,7 @@ const routes: RouteRecordRaw[] = [
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
     children: [
-      { path: '', component: () => import('src/pages/index.vue'), meta: { bgHeader: 'home' } },
+      { path: '', component: () => import('src/pages/home.vue'), meta: { bgHeader: 'home' } },
       { path: '/apparel', component: () => import('src/pages/apparel.vue'), meta: { bgHeader: 'apparel' } },
       { path: '/entertainment', component: () => import('src/pages/entertainment.vue'), meta: { bgHeader: 'entertainment' } },
       { path: '/justice', component: () => import('src/pages/justice.vue'), meta: { bgHeader: 'justice' } },

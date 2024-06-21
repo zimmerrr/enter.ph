60b7d55398339b2abecc053f9c27ddf7a42a4dd9
zimmer <zimmertorrijos@gmail.com>
2024-06-21 23:06:49 +0800

    add article, infographics, header constants

--- src/components/constants.ts
+++ src/components/constants.ts
@@ -1,3 +1,19 @@
+import EntertainmentHeaderImg from 'src/assets/home/entertainment.png'
+import ApparelHeaderImg from 'src/assets/home/apparel.png'
+import JusticeHeadercImg from 'src/assets/home/justice.png'
+
+import EntertainmentArticleImg from 'src/assets/article/entertainment.png'
+import ApparelArticleImg from 'src/assets/article/apparel.png'
+import JusticeArticleImg from 'src/assets/article/justice.png'
+
+// import EntertainmentVideo from 'src/assets/video/entertainment.mp4'
+// import ApparelVideo from 'src/assets/video/apparel.mp4'
+// import JusticeVideo from 'src/assets/video/justice.mp4'
+
+import EntertainmentInfographicImg from 'src/assets/infographics/entertainment.png'
+import ApparelInfographicImg from 'src/assets/infographics/apparel.png'
+import JusticeInfographicImg from 'src/assets/infographics/justice.png'
+
 export const MENU = [
   {
     name: 'Home',
@@ -87,5 +103,68 @@ export const DEVELOPERS = [
     key: 'project_manager',
     name: 'Project Manager',
     members: ['Svet Jazmine Dio'],
+  },
+]
+
+export const HEADER = [
+  {
+    name: 'Entertainment',
+    image: EntertainmentHeaderImg,
+    description: 'dedicated to showcasing the stories and talents of local apparel workers in the Philippines. Discover their craft, learn about their impact, and support the heart of the Philippine fasion industry.',
+  },
+  {
+    name: 'Apparel',
+    image: ApparelHeaderImg,
+    description: 'dedicated to showcasing the stories and talents of local apparel workers in the Philippines. Discover their craft, learn about their impact and support the heart of the Philippine fasion industry.',
+  },
+  {
+    name: 'Justice',
+    image: JusticeHeadercImg,
+    description: 'Your hub for local justice advocay in the Philippines. Stay informed about key issues, meet inspiring advocates, and join the movement for a fair and just society',
+  },
+]
+
+export const ARTICLE = [
+  {
+    name: 'Entertainment',
+    image: EntertainmentHeaderImg,
+    description1: 'The Korean Wave, or Hallyu, began in the 1990s in East Asia and expanded globally, impacting regions like the United States, Latin America, and Europe. Due to strong foundations',
+    description2: "supported by the cultural policies of South Korea, Hallyu has become a symbol of Korean cultural success and a tool for enhancing national image. The rise of K-pop can be traced to South Korea's democratic reforms in 1987, which liberalized media and fostered cultural innovation. Groups like Seo Taiji and Boys pioneered a new era of music, challenging traditional norms and laying the groundwork for K-pop's global rise.",
+    description3: "The technological advancements, particularly in digital platforms like YouTube, facilitated K-pop's international reach and influence beyond Asia, shaping global pop culture dynamics. In the Philippines, K-pop gained popularity first through dramas and later through music, leading to a dedicated fanbase. This influence sparked the emergence of P-pop, blending local and Korean pop elements in groups like MNL48 and SB19, which have achieved both local success and international recognition.",
+    description4: "The Korean Wave continues to reshape global pop culture, particularly in the Philippines, where it has inspired local adaptations and collaborations. As Filipino artists integrate K-pop's standards and aesthetics with their cultural expressions, the future promises further innovation and cultural exchange. This dynamic relationship between Korean and Filipino pop cultures highlights the enduring influence and transformative power of Hallyu on a global scale.",
+  },
+  {
+    name: 'Apparel',
+    image: ApparelHeaderImg,
+    description1: 'Urbanization significantly impacts rural communities, with positive and negative effects on rural garment workers, particularly regarding finances, social life, environment, health, and culture. The opening of garment factories in rural areas creates jobs, reducing',
+    description2: "poverty and increasing wages. However, these wages are often lower than in cities and can be offset by rising living costs. This economic disparity makes financial stability difficult, necessitating policies for fair wages and workers' rights protection. Urbanization also leads to the migration of young people to cities, resulting in a decline in rural populations and fragmented families, which strains rural economies and leaves older residents without support.",
+    description3: 'While it can enhance education and job opportunities, traditional family structures can be disrupted, leaving older adults and children neglected for. Therefore, support is crucial for families to adapt to these changes. Furthermore, garment factories often exploit local resources and cause pollution, damaging local ecosystems and farming. Sustainable practices and stringent environmental regulations are needed to mitigate these negative effects and ensure long-term ecological health.',
+    description4: 'The complex impacts of urbanization on rural garment workers require a balanced approach to ensure benefits without harm. Policies promoting fair wages, safe working conditions, healthcare, education access, and environmental protection are vital for balanced and inclusive growth, aiming for a fair and prosperous future for rural communities.',
+  },
+  {
+    name: 'Justice',
+    image: JusticeHeadercImg,
+    description1: 'Gender Justice has been a longstanding advocacy and as more emphasized in the contemporary world. All over the globe, a cornucopia of human rights violations were recorded in relation to gender discrimination. The United Nations Sustainable Development Goals',
+    description2: 'were concurred upon by all United Nations Member states in 2015. According to Global Citizen Facts, a third of countries have made no progress towards achieving Gender Equality; 18 countries have portrayed worsening cases of gender inequality in Venezuela, Afghanistan, Algeria, Belarus, Kuwait, and Ecuador.',
+    description3: 'According to the Philippine Institute for Development Studies (PIDS), the Philippines has improved slightly in 2023 appertaining to the context of gender equality, advancing three spots to the 16th ranking among 146 countries in the latest World Economic Forum’s (WEF) Global Gender Gap Index report (GGGR). Meanwhile, it is also paramount to point out that the Gender Gap Index focuses on the measure of achievement, as it assesses gender gaps but not the norms and beliefs. A 2017 report from the Philippine Institute for Development Studies emphasized the underrepresentation of women in governance over the last few decades, the overall picture of female representation was then marked “particularly alarming”.',
+    description4: 'The Commission on Human Rights in the Philippines advocates the SOGIE Equality bill as part of the measurement for law-driven justice for gender discrimination. According to the Psychological Association of the Philippines, lesbian, gay, bisexual, and transgender Filipinos continue to experience stigma, prejudice, and discrimination in Philippine Society. The stigma is prevalently manifested through bullying, teasing, and harassment. In the view of Amnesty Philippines, the SOGIE Equality Bill recognizes the fundamental rights of every person regardless of sexual orientation and gender identity to address discrimination against the young LGBTQIA+ and everyone in general.',
+  },
+]
+
+export const INFOGRAPHICS = [
+  {
+    name: 'Entertainment',
+    image: EntertainmentInfographicImg,
+    description: "K-Pop's rise to global prominence, known as Hallyu or the Korean wave, has significantly impacted the Philippine music scene. This infographic highlights the key milestones and transformations in Philippine popular music influenced by K-Pop from the 1990s to the present.",
+  },
+  {
+    name: 'Apparel',
+    image: ApparelInfographicImg,
+    description: "Urbanization impacts society in numerous ways, presenting both challenges and opportunities. This infographics explores key issues like Mental Health, Sustainable Cities, Salary Range, Apparel Worker conditions, Women's status, and Social dynamics, highlighting the need for inclusive growth, fair wages, gender equality, and sustainable urban development.",
+  },
+  {
+    name: 'Justice',
+    image: JusticeInfographicImg,
+    description: 'Gender discrimination remains a significant global issue, necessitating a comprehensive approach to achieve justice. This infographics highlights essential areas such as Gender Equality, Empowerment, Norms and Roles, Diversity Across Culture, Education and Awareness, and Respect. These components are crucial in fostering an equitable society where everyone can live free from discrimination.',
   },
 ]

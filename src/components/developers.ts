75ee77ea1a1e8a15ab683bb5913bedebf6762cc9
svetdio <122599561+svetdio@users.noreply.github.com>
2024-06-23 00:49:48 +0800

    about us directory

---
+++ src/components/developers.ts
@@ -0,0 +1,195 @@
+import Torrijos from 'src/assets/developers/30.png'
+import Manibog from 'src/assets/developers/33.png'
+import Dio from 'src/assets/developers/31.png'
+import Joaquin from 'src/assets/developers/01.png'
+import Sabater from 'src/assets/developers/02.png'
+import Agarma from 'src/assets/developers/03.png'
+import Garcia from 'src/assets/developers/04.png'
+import Magdaraog from 'src/assets/developers/05.png'
+import Delgado from 'src/assets/developers/06.png'
+import Ramos from 'src/assets/developers/07.png'
+import Palen from 'src/assets/developers/08.png'
+import Gregorio from 'src/assets/developers/09.png'
+import Borbe from 'src/assets/developers/10.png'
+import Rubio from 'src/assets/developers/11.png'
+import Cuyno from 'src/assets/developers/12.png'
+import Zoleta from 'src/assets/developers/13.png'
+import Cruzet from 'src/assets/developers/14.png'
+import Sanggalang from 'src/assets/developers/15.png'
+import Borja from 'src/assets/developers/16.png'
+import Wahing from 'src/assets/developers/28.png'
+import Costa from 'src/assets/developers/18.png'
+import DelRosario from 'src/assets/developers/19.png'
+import Mercado from 'src/assets/developers/20.png'
+import Ogot from 'src/assets/developers/21.png'
+import Macasiab from 'src/assets/developers/22.png'
+import DelaCruz from 'src/assets/developers/23.png'
+import Ojano from 'src/assets/developers/24.png'
+import Estribor from 'src/assets/developers/25.png'
+import Macapobre from 'src/assets/developers/26.png'
+import Regalado from 'src/assets/developers/27.png'
+import Arcangel from 'src/assets/developers/17.png'
+import Naga from 'src/assets/developers/29.png'
+
+export const ABOUT = [
+  {
+    name: 'Svet Jazmine Dio',
+    role: 'Project Manager/QA',
+    image: Dio,
+  },
+  {
+    name: 'Christian Andrei Torrijos',
+    role: 'Web Developer',
+    image: Torrijos,
+  },
+  {
+    name: 'Franz Emsley Manibog',
+    role: 'Web Developer',
+    image: Manibog,
+  },
+  {
+    name: 'Jerome Joaquin',
+    role: 'Content Creator',
+    image: Joaquin,
+  },
+  {
+    name: 'Kervin Lance Sabater',
+    role: 'Content Creator',
+    image: Sabater,
+  },
+  {
+    name: 'Jeffrey Agarma',
+    role: 'Content Creator',
+    image: Agarma,
+  },
+  {
+    name: 'Trisha Grace Garcia',
+    role: 'Content Creator',
+    image: Garcia,
+  },
+  {
+    name: 'Marian Magdaraog',
+    role: 'Content Creator',
+    image: Magdaraog,
+  },
+  {
+    name: 'Dave Delgado',
+    role: 'Content Creator',
+    image: Delgado,
+  },
+  {
+    name: 'Jolo Marco Ramos',
+    role: 'Content Creator',
+    image: Ramos,
+  },
+  {
+    name: 'Kerchei Palen',
+    role: 'Content Creator',
+    image: Palen,
+  },
+  {
+    name: 'Genesis Nino Gregorio',
+    role: 'Content Creator',
+    image: Gregorio,
+  },
+  {
+    name: 'Elaine Borbe',
+    role: 'Content Creator',
+    image: Borbe,
+  },
+  {
+    name: 'Marbie Jean Rubio',
+    role: 'Content Creator',
+    image: Rubio,
+  },
+  {
+    name: 'Cristina Rose Cuyno',
+    role: 'Content Creator',
+    image: Cuyno,
+  },
+  {
+    name: 'Keice Zoleta',
+    role: 'Content Creator',
+    image: Zoleta,
+  },
+  {
+    name: 'Mariela Cruzet',
+    role: 'Content Creator',
+    image: Cruzet,
+  },
+  {
+    name: 'Angela Sanggalang',
+    role: 'Content Creator',
+    image: Sanggalang,
+  },
+  {
+    name: 'Maria Crisanta Borja',
+    role: 'Content Creator',
+    image: Borja,
+  },
+  {
+    name: 'Reychelle Wahing',
+    role: 'Web Designer',
+    image: Wahing,
+  },
+  {
+    name: 'Alex Costa Jr',
+    role: 'Web Designer',
+    image: Costa,
+  },
+  {
+    name: 'Arnie Monica Del Rosario',
+    role: 'Web Designer',
+    image: DelRosario,
+  },
+  {
+    name: 'Van Ellis Mercado',
+    role: 'Web Designer',
+    image: Mercado,
+  },
+  {
+    name: 'Gerwin Jahziel Ogot',
+    role: 'Web Designer',
+    image: Ogot,
+  },
+  {
+    name: 'John Hendrix Macasiab',
+    role: 'Web Designer',
+    image: Macasiab,
+  },
+  {
+    name: 'Jovelyn Dela Cruz',
+    role: 'Web Designer',
+    image: DelaCruz,
+  },
+  {
+    name: 'Jose Christopher Ojano',
+    role: 'Web Designer',
+    image: Ojano,
+  },
+  {
+    name: 'Charnelle Estribor',
+    role: 'Web Designer',
+    image: Estribor,
+  },
+  {
+    name: 'Rommel Macapobre',
+    role: 'Web Designer',
+    image: Macapobre,
+  },
+  {
+    name: 'Niño Regalado',
+    role: 'Web Designer',
+    image: Regalado,
+  },
+  {
+    name: 'Ailyn Arcangel',
+    role: 'Web Designer',
+    image: Arcangel,
+  },
+  {
+    name: 'Rodney Naga',
+    role: 'Web Designer',
+    image: Naga,
+  },
+]

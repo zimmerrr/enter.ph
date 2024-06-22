4b5726898b59d9693b7b76fb991b8203bd13f1a0
zimmer <zimmertorrijos@gmail.com>
2024-06-23 02:59:33 +0800

    update constants

--- src/components/constants.ts
+++ src/components/constants.ts
@@ -1,6 +1,6 @@
-import EntertainmentHeaderImg from 'src/assets/home/entertainment.png'
-import ApparelHeaderImg from 'src/assets/home/apparel.png'
-import JusticeHeadercImg from 'src/assets/home/justice.png'
+import EntertainmentHeaderImg from 'src/assets/icons/entertainment.png'
+import ApparelHeaderImg from 'src/assets/icons/apparel.png'
+import JusticeHeadercImg from 'src/assets/icons/justice.png'
 
 import EntertainmentArticleImg from 'src/assets/article/entertainment.png'
 import ApparelArticleImg from 'src/assets/article/apparel.png'
@@ -130,7 +130,8 @@ export const HEADER = [
 export const ARTICLE = [
   {
     name: 'Entertainment',
-    image: EntertainmentHeaderImg,
+    image: EntertainmentArticleImg,
+    title: 'Globalizing Inspirations: K-Pop and its Influence on Philippine Popular Music ',
     description1: 'The Korean Wave, or Hallyu, began in the 1990s in East Asia and expanded globally, impacting regions like the United States, Latin America, and Europe. Due to strong foundations',
     description2: "supported by the cultural policies of South Korea, Hallyu has become a symbol of Korean cultural success and a tool for enhancing national image. The rise of K-pop can be traced to South Korea's democratic reforms in 1987, which liberalized media and fostered cultural innovation. Groups like Seo Taiji and Boys pioneered a new era of music, challenging traditional norms and laying the groundwork for K-pop's global rise.",
     description3: "The technological advancements, particularly in digital platforms like YouTube, facilitated K-pop's international reach and influence beyond Asia, shaping global pop culture dynamics. In the Philippines, K-pop gained popularity first through dramas and later through music, leading to a dedicated fanbase. This influence sparked the emergence of P-pop, blending local and Korean pop elements in groups like MNL48 and SB19, which have achieved both local success and international recognition.",
@@ -138,7 +139,8 @@ export const ARTICLE = [
   },
   {
     name: 'Apparel',
-    image: ApparelHeaderImg,
+    image: ApparelArticleImg,
+    title: 'The Realities of  Urbanization',
     description1: 'Urbanization significantly impacts rural communities, with positive and negative effects on rural garment workers, particularly regarding finances, social life, environment, health, and culture. The opening of garment factories in rural areas creates jobs, reducing',
     description2: "poverty and increasing wages. However, these wages are often lower than in cities and can be offset by rising living costs. This economic disparity makes financial stability difficult, necessitating policies for fair wages and workers' rights protection. Urbanization also leads to the migration of young people to cities, resulting in a decline in rural populations and fragmented families, which strains rural economies and leaves older residents without support.",
     description3: 'While it can enhance education and job opportunities, traditional family structures can be disrupted, leaving older adults and children neglected for. Therefore, support is crucial for families to adapt to these changes. Furthermore, garment factories often exploit local resources and cause pollution, damaging local ecosystems and farming. Sustainable practices and stringent environmental regulations are needed to mitigate these negative effects and ensure long-term ecological health.',
@@ -146,11 +148,30 @@ export const ARTICLE = [
   },
   {
     name: 'Justice',
-    image: JusticeHeadercImg,
+    image: JusticeArticleImg,
+    title: 'Gender Discrimination and Path to Justice',
     description1: 'Gender Justice has been a longstanding advocacy and as more emphasized in the contemporary world. All over the globe, a cornucopia of human rights violations were recorded in relation to gender discrimination. The United Nations Sustainable Development Goals',
     description2: 'were concurred upon by all United Nations Member states in 2015. According to Global Citizen Facts, a third of countries have made no progress towards achieving Gender Equality; 18 countries have portrayed worsening cases of gender inequality in Venezuela, Afghanistan, Algeria, Belarus, Kuwait, and Ecuador.',
     description3: 'According to the Philippine Institute for Development Studies (PIDS), the Philippines has improved slightly in 2023 appertaining to the context of gender equality, advancing three spots to the 16th ranking among 146 countries in the latest World Economic Forum’s (WEF) Global Gender Gap Index report (GGGR). Meanwhile, it is also paramount to point out that the Gender Gap Index focuses on the measure of achievement, as it assesses gender gaps but not the norms and beliefs. A 2017 report from the Philippine Institute for Development Studies emphasized the underrepresentation of women in governance over the last few decades, the overall picture of female representation was then marked “particularly alarming”.',
     description4: 'The Commission on Human Rights in the Philippines advocates the SOGIE Equality bill as part of the measurement for law-driven justice for gender discrimination. According to the Psychological Association of the Philippines, lesbian, gay, bisexual, and transgender Filipinos continue to experience stigma, prejudice, and discrimination in Philippine Society. The stigma is prevalently manifested through bullying, teasing, and harassment. In the view of Amnesty Philippines, the SOGIE Equality Bill recognizes the fundamental rights of every person regardless of sexual orientation and gender identity to address discrimination against the young LGBTQIA+ and everyone in general.',
+  },
+]
+
+export const VIDEO = [
+  {
+    title: 'Entertainment in Globalization of K-Pop',
+    video: 'https://www.youtube.com/embed/ptmOGI1Dsyc',
+    description: '',
+  },
+  {
+    title: 'Apparel Workers in Urbanization',
+    video: 'https://www.youtube.com/embed/rbfaK7Y6GEI',
+    description: 'Urbanization and the Apparel Workers',
+  },
+  {
+    title: 'Justice Advocates for Gender Discrimination',
+    video: 'https://www.youtube.com/embed/tUcTr3qxdx8',
+    description: '',
   },
 ]
 
@@ -171,3 +192,9 @@ export const INFOGRAPHICS = [
     description: 'Gender discrimination remains a significant global issue, necessitating a comprehensive approach to achieve justice. This infographics highlights essential areas such as Gender Equality, Empowerment, Norms and Roles, Diversity Across Culture, Education and Awareness, and Respect. These components are crucial in fostering an equitable society where everyone can live free from discrimination.',
   },
 ]
+
+export const ABOUT = {
+  description1: 'Welcome to ENTER.PH, a dynamic platform created by the Bachelor of Science in Computer Science Section 4A of Cavite State University - Imus Campus. This website was developed as part of our coursework for the subject "The Contemporary World" (Subject Code: GNED 7).',
+  description2: 'At ENTER.PH, we aim to provide a comprehensive look into the vibrant spheres of local entertainment, justice advocacy, and the apparel industry in the Philippines. Our goal is to inform, inspire, and connect our community through engaging content and meaningful stories.',
+  description3: 'Join us as we explore and celebrate the unique aspects of Philippine culture and society, driven by the passion and dedication of future tech innovators and advocates.',
+}
